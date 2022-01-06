553c33410b2a06a0daf62aec5a7bd3efdf25b60a
vladimir360 <vladimir360@mail.ru>
2022-01-06 20:58:01 +0300

    add store but layout has error

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem :products="products"/>
+  </ul>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue"
+
+export default {
+    props: ['products'],
+    components: {
+        ProductItem
+    }
+}
+
+</script>
